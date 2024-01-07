b741b8cf50eefbd624d3c84dd8f3e7b582e0d0a9
Dipesh Regmi <dipeshregmi999@gmail.com>
2024-01-07 18:32:44 +0545

    new one just for git learning

---
+++ Project1/data.hpp
@@ -0,0 +1,28 @@
+#ifdef __DATA_H
+#define __DATA_H
+
+#include<vector>
+#include<stdint.h>
+#include<stdio.h>
+
+class data {
+	std::vector<uint8_t>* feature_vector;  // no class at end
+	uint8_t label;
+	int enum_label; // 
+
+public:
+	void set_feature_vector(std::vector<uint8_t>*);
+	void append_to_feature_vector(uint* _t);
+	void set_label(uint8_t);
+	void set_enumerated_label(int);
+
+	int get_feature_vector_size();
+	unit8_t get_label();
+	uint8_t get_enumerated_label();
+	uint8_t get_enumerated_label();
+
+	std::vector<uint8_t> * get_feature_vector();
+
+};
+#endif // 
+
---
+++ Project1/data_handler.hpp
@@ -0,0 +1,35 @@
+#ifndef _DATA_HANDLER_H
+#define _DATA_HANDLER_H
+#include<iostream>
+#include<fstream>
+#include "stdint.h"
+#include "data.hpp"
+#include<vector>
+#include<string>
+#include<map>
+#include<unordered_set>
+
+class data_handler {
+	std::vector<data*>* data_array;
+	std::vector<data*>* training_data;
+	std::vector<data*>* test_data;
+	std::vector<data*>* validation_data;
+	lua_
+	int num_classes;
+	int feature_vector_size;
+	std::map<uint8_t, int> class_map;
+
+	const double TRAIN_SET_PERCENT = 0.75;
+	const double TEST_SET_PERCENT = 0.20;
+	const double VALIDATION_PERCENT = 0.05;
+
+public:
+	data_handler();
+	~data_handler();
+
+	void read_feature_vector(std::string path);
+	void read_feature_labels(std::string path);
+
+};
+
+#endif // !_DATA_HANDLER_H
